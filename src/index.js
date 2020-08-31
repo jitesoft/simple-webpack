1c94a1468f3e4cbf9be5c6894b63c2b4dbf9123f
Johannes Tegnér <johannes@jitesoft.com>
2020-08-31 13:23:53 +0200

    Removed src directory, no longer used!
    Signed-off-by: Johannes Tegnér <johannes@jitesoft.com>

--- src/index.js
+++
@@ -1,7 +0,0 @@
-// This file should just include index.js and index.css to allow a single point of entry.
-// Why? Cause it makes it easier for you!
-
-import './style/index.scss';
-import './js/index.js';
-
-// There we go! All done, now it builds! ;)
