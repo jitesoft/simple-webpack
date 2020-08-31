cefd6963bb2c7ba3796f991cc963950be6c0f6a9
Johannes Tegnér <johannes@jitesoft.com>
2020-08-31 13:29:59 +0200

    Missing )!
    Signed-off-by: Johannes Tegnér <johannes@jitesoft.com>

--- init.js
+++ init.js
@@ -53,7 +53,7 @@ const run = async () => {
   await fs.open(path.resolve(process.cwd(), 'src', 'style', 'index.scss'), flags.O_CREAT | flags.O_RDWR);
   await fs.open(path.resolve(process.cwd(), 'src', 'js', 'index.js'), flags.O_CREAT | flags.O_RDWR);
 
-  if (exist(path.resolve(process.cwd(), 'src', 'index.js')) {
+  if (exist(path.resolve(process.cwd(), 'src', 'index.js'))) {
     console.log(`Failed to create ${path.resolve(process.cwd(), 'src', 'index.js')}, file already exist.`);
   } else {
     await fs.copyFile(path.resolve(__dirname, 'src', 'index.js'), path.resolve(process.cwd(), 'src', 'index.js'));
